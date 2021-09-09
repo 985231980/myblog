270b33e582d0b266f198ffb7111481a408fff8b4
Ding <985231980@qq.com>
2021-09-09 19:35:38 +0800

    博客页面修改

--- src/components/basePanel.vue
+++ src/components/basePanel.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    <div>
+    <div style="font-family:'Microsoft JhengHei'">
       <div class="left_side space">.</div>
       <div class="blog">
         <el-card class="box-card blog_card">
           <div slot="header" class="clearfix">
-            <span>博客</span>
+            <span style="font-weight: 900">博客</span>
             <span style="float: right; padding: 3px 0">共<span style="color: #E6A23C">1</span>篇</span>
           </div>
           <div>
+            <div class="blog_layout" v-for="o in 4" :key="o">
+              <div3><img src="../assets/test/test1.jpg"></div3>
+              <div1>土耳其小孩提前剪彩被总统敲头</div1>
+              <div2>
+                9月4日，土耳其总统埃尔多安参加活动的剪彩仪式时被一个小男孩抢先一步剪断了彩带。
+                埃尔多安开玩笑地用指关节敲了敲小男孩的头，提醒他提前剪断了彩带。9月4日
+                ，土耳其总统埃尔多安参加活动的剪彩仪式时被一个小男孩抢先一步剪断了彩
+              </div2>
 
+              <div4>
+                <img class="avatar" src="../assets/iconfont/user.png">
+                <table1>
+                  <tr><td>测试作者</td></tr>
+                  <tr><td class="timeSize">2021/09/09</td></tr>
+                </table1>
+                <div><table><tr>
+                      <td><img src="../assets/iconfont/look.png" style="width: 1.4rem"></td>
+                      <td>123</td>
+                      <td><img src="../assets/iconfont/like.png" class="layoutImg" style="width: 1.2rem"></td>
+                      <td class="layoutImgText">23</td>
+                    </tr></table></div>
+              </div4>
+              <div style="clear: both"></div><hr>
+            </div>
           </div>
         </el-card>
       </div>
@@ -63,33 +86,116 @@ export default {
     user-select: none;
     color: #e3e3e5;
   }
+  .blog_layout{
+    padding:1.6rem 2rem 1.2rem 2rem;/*上右下左*/
+  }
+  .blog_layout div1{/*title*/
+    float: left;
+    width: 54%;
+    height: 2rem;
+    font-weight: 900;
+    font-size: 1.2rem;
+    display: -webkit-box!important;
+    -webkit-box-orient: vertical!important;
+    -webkit-line-clamp: 1!important;
+    overflow: hidden!important;
+  }
+  .layoutImg{
+    margin-left: 1rem;
+  }
+  .blog_layout div2{/*text*/
+    float: left;
+    width: 54%;
+    min-height: 10rem;
+    line-height: 2rem;
+    font-size: 1rem;
+    max-height: 10rem;
+    display: -webkit-box!important;
+    -webkit-box-orient: vertical!important;
+    -webkit-line-clamp: 5!important;
+    overflow: hidden!important;
+  }
+  .blog_layout div3 img{/*img*/
+    float: right;
+    width: 42%;
+    border-radius: 1rem;
+    height: 16rem;
+  }
+  .blog_layout div4{/*text*/
+    float: left;
+    width: 54%;
+    height: 3rem;
+    margin-top: 1rem;
+  }
+  .blog_layout div4 img{
+    float: left;
+    width: 3rem;
+    line-height: 4rem;
+    vertical-align: middle;
+  }
+  .blog_layout div4 table1 td{
+    float: left;
+    font-size: 1rem;
+    margin-top: 0.2rem;
+    margin-left: 0.4rem;
+    max-width: 6rem!important;
+    display: -webkit-box!important;
+    -webkit-box-orient: vertical!important;
+    -webkit-line-clamp: 1!important;
+    overflow: hidden!important;
+  }
+  .timeSize{
+    font-size: 0.8rem!important;
+    color: #777777;
+  }
+  .blog_layout div4 table{
+    float: left;
+  }
+  .blog_layout div4 div{
+    font-weight: 500!important;
+    font-size: 1rem!important;
+    float: right;
+    width: 40%;
+  }
+  .blog_layout div4 div table img{
+    width: 1.6rem;
+  }
+  .blog_layout div4 div table{
+    width: 1.6rem;
+    margin-top: 0.6rem;
+  }
+  .blog_layout hr{
+    background: #ebeef5;
+    width: 100%;
+    margin-top: 3rem;
+  }
   .blog_card{
     min-height: 38rem;
-    max-height: 80rem;
+    /*max-height: 80rem;*/
     margin: auto;
     width: 96%;
     border: 0px!important;
     box-shadow: 0 0 1rem #777777!important;
     border-radius: 0.8rem!important;
-    overflow: auto!important;
+    /*overflow: auto!important;*/
   }
-  .blog_card::-webkit-scrollbar {
-    /*滚动条整体样式*/
-    width : 1rem;  /*高宽分别对应横竖滚动条的尺寸*/
-    height: 1rem;
-  }
-  .blog_card::-webkit-scrollbar-thumb {
-    /*滚动条里面小方块*/
-    border-radius: 1rem;
-    box-shadow   : inset 0 0 1rem #777777;
-    background   : #777777;
-  }
-  .blog_card::-webkit-scrollbar-track {
-    /*滚动条里面轨道*/
-    box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);
-    border-radius: 1rem;
-    background   : #ededed;
-  }
+  /*.blog_card::-webkit-scrollbar {*/
+  /*  !*滚动条整体样式*!*/
+  /*  width : 1rem;  !*高宽分别对应横竖滚动条的尺寸*!*/
+  /*  height: 1rem;*/
+  /*}*/
+  /*.blog_card::-webkit-scrollbar-thumb {*/
+  /*  !*滚动条里面小方块*!*/
+  /*  border-radius: 1rem;*/
+  /*  box-shadow   : inset 0 0 1rem #777777;*/
+  /*  background   : #777777;*/
+  /*}*/
+  /*.blog_card::-webkit-scrollbar-track {*/
+  /*  !*滚动条里面轨道*!*/
+  /*  box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);*/
+  /*  border-radius: 1rem;*/
+  /*  background   : #ededed;*/
+  /*}*/
   .right_card{
     margin: auto;
     width: 96%;
@@ -109,8 +215,77 @@ export default {
     .right_side{
       display: none;
     }
+    /*修改风格*/
+    .blog_layout div3 img{/*img*/
+      width: 100%;
+      border-radius: 1rem;
+      height: 16rem;
+    }
+    .blog_layout div1{/*title*/
+      margin-top: 0.8rem;
+      float: left;
+      width: 100%;
+      height: 2rem;
+      font-weight: 900;
+      font-size: 1.2rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div2{/*text*/
+      float: left;
+      width: 100%;
+      min-height: 6rem;
+      line-height: 2rem;
+      font-size: 1rem;
+      max-height: 6rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 3!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4{/*text*/
+      float: left;
+      width: 100%;
+      height: 3rem;
+      margin-top: 1rem;
+    }
+    .blog_layout div4 img{
+      float: left;
+      width: 3rem;
+      line-height: 4rem;
+      vertical-align: middle;
+    }
+    .blog_layout div4 table{
+      float: left;
+    }
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 20%;
+    }
+    .blog_layout div4 div table img{
+      width: 1.6rem;
+    }
+    .blog_layout div4 div table{
+      width: 1.6rem;
+      margin-top: 0.6rem;
+    }
   }
   @media screen and (max-width: 800px) {
+    .blog_layout div4 table1 td{
+
+      float: left;
+      font-size: 1rem;
+      margin-top: 0.1rem;
+      max-width: 20rem!important;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
     .left_side{
       width: 2%!important;
       float: left;
@@ -121,6 +296,232 @@ export default {
     }
     .right_side{
       display: none;
+    }
+    .blog_layout div3 img{/*img*/
+      width: 100%;
+      border-radius: 1rem;
+      height: 14rem;
+    }
+    .blog_layout div1{/*title*/
+      margin-top: 0.8rem;
+      float: left;
+      width: 100%;
+      height: 2rem;
+      font-weight: 900;
+      font-size: 1.2rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div2{/*text*/
+      float: left;
+      width: 100%;
+      min-height: 4rem;
+      line-height: 1.4rem;
+      font-size: 1rem;
+      max-height: 4rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 3!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 30%;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    .blog_layout hr{
+      background: #ebeef5;
+      width: 100%;
+      margin-top: 1.2rem;
+      margin-bottom: 0.6rem;
+    }
+    .blog_layout{
+      padding:0.8rem 0.8rem 0.8rem 0.8rem;/*上右下左*/
+    }
+    .blog_layout div3 img{/*img*/
+      width: 100%;
+      border-radius: 1rem;
+      height: 12rem;
+    }
+    .blog_layout div1{/*title*/
+      margin-top: 0.8rem;
+      float: left;
+      width: 100%;
+      height: 1.4rem;
+      font-weight: 900;
+      font-size: 1.2rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+    .timeSize{
+      font-size: 0.6rem!important;
+      color: #777777;
+    }
+    .blog_layout div2{/*text*/
+      margin-top: 0.5rem;
+      float: left;
+      width: 100%;
+      min-height: 4rem;
+      line-height: 1.4rem;
+      font-size: 1rem;
+      max-height: 4rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 3!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 36%;
+    }
+    .blog_layout div4 table1 td{
+      float: left;
+      font-size: 1rem;
+      margin-top: 0.1rem;
+      max-width: 8rem!important;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .blog_layout hr{
+      background: #ebeef5;
+      width: 100%;
+      margin-top: 2rem;
+      margin-bottom: 0.8rem;
+    }
+    .blog_layout{
+      padding:0.4rem 0.4rem 0.4rem 0.4rem;/*上右下左*/
+    }
+    .blog_layout div2{/*text*/
+      margin-top: 0.5rem;
+      float: left;
+      width: 100%;
+      min-height: 4rem;
+      line-height: 1.4rem;
+      font-size: 1rem;
+      max-height: 4rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 3!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 36%;
+    }
+    .blog_layout div4{/*text*/
+      float: left;
+      width: 100%;
+      height: 1rem;
+      margin-top: 1rem;
+    }
+    .blog_layout div4 table1 td{
+      float: left;
+      font-size: 0.8rem;
+      max-width: 6rem!important;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div img{
+      width: 1.4rem!important;
+    }
+    .layoutImg{
+      margin-left: 0.4rem;
+    }
+    .avatar{
+      width: 2.4rem!important;
+    }
+  }
+  @media screen and (max-width: 360px) {
+    .blog_layout hr{
+      background: #ebeef5;
+      width: 100%;
+      margin-top: 2rem;
+      margin-bottom: 0.8rem;
+    }
+    .blog_layout{
+      padding:0.4rem 0.4rem 0.4rem 0.4rem;/*上右下左*/
+    }
+    .blog_layout div3 img{/*img*/
+      width: 100%;
+      border-radius: 1rem;
+      height: 10rem;
+    }
+    .blog_layout div2{/*text*/
+      margin-top: 0.5rem;
+      float: left;
+      width: 100%;
+      min-height: 4rem;
+      line-height: 1.4rem;
+      font-size: 1rem;
+      max-height: 4rem;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 3!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 42%;
+    }
+    .blog_layout div4{/*text*/
+      float: left;
+      width: 100%;
+      height: 1rem;
+      margin-top: 1rem;
+    }
+    .blog_layout div4 table1 td{
+      float: left;
+      font-size: 0.8rem;
+      max-width: 4rem!important;
+      display: -webkit-box!important;
+      -webkit-box-orient: vertical!important;
+      -webkit-line-clamp: 1!important;
+      overflow: hidden!important;
+    }
+    .blog_layout div4 div img{
+      width: 1.4rem!important;
+    }
+    .avatar{
+      width: 2.4rem!important;
+    }
+  }
+
+  @media screen and (max-width: 290px){
+    .blog_layout div4 div{
+      font-weight: 500!important;
+      font-size: 1rem!important;
+      float: right;
+      width: 40%;
+    }
+    .layoutImg{
+     display: none;
+    }
+    .layoutImgText{
+      display: none;
+    }
+    .blog_layout div3 img{/*img*/
+      width: 100%;
+      border-radius: 1rem;
+      height: 8rem;
     }
   }
 </style>
